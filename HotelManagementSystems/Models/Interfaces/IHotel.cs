d80d31fa3decb78b14392c761227292753cb1d03
Regan Dufort <regan.t.dufort@gmail.com>
2019-01-29 11:48:12 -0800

    Implemented Hotel interface

---
+++ HotelManagementSystems/Models/Interfaces/IHotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystems.Models.Interfaces
+{
+    interface IHotelManager
+    {
+        // create a new hotel
+        void CreateHotel(Hotel hotel);
+
+        // read details of a hotel
+        Hotel GetHotel(int id);
+
+        // update a hotel
+        void UpdateHotel(Hotel hotel);
+
+        // delete a hotel
+        void DeleteHotel(Hotel hotel);
+
+        void DeleteHotel(int id);
+    }
+}
---
+++ HotelManagementSystems/Models/Services/HotelManagementService.cs
@@ -0,0 +1,36 @@
+﻿using HotelManagementSystems.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelManagementSystems.Models.Services
+{
+    public class HotelManagementService : IHotelManager
+    {
+        public void CreateHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hotel GetHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
