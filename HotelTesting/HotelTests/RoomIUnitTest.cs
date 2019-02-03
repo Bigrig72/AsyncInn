da5dfc5b94162de1f3ecbb9ded2887218ceae80a
Regan Dufort <regan.t.dufort@gmail.com>
2019-02-02 17:32:09 -0800

    Finished testing for rooms and deployed link will still not render my pages

--- HotelTesting/HotelTests/RoomIUnitTest.cs
+++ HotelTesting/HotelTests/RoomIUnitTest.cs
@@ -22,6 +22,14 @@ namespace HotelTesting.HotelTests
             Assert.Equal(1, roomA.ID);
         }
         [Fact]
+        public void SetRoomId()
+        {
+            Room roomA = new Room();
+            roomA.ID = 1;
+            roomA.ID = 2;
+            Assert.Equal(2, roomA.ID);
+        }
+        [Fact]
         public void GetRoomName()
         {
             Room roomA = new Room();
@@ -29,12 +37,114 @@ namespace HotelTesting.HotelTests
             Assert.Equal("standard", roomA.Name);
         }
         [Fact]
-        public void GetRoomAmenityFromAmenitites()
+        public void SetRoomName()
         {
             Room roomA = new Room();
-            roomA.RoomAmentities.Amentities.Name = "Microwave";
-            Assert.Equal("Microwave", roomA.RoomAmentities.Amentities.Name);
+            roomA.Name = "standard";
+            roomA.Name = "Big";
+            Assert.Equal("Big", roomA.Name);
+        }
+        [Fact]
+        public void GetRoomAmenitiesID()
+        {
+            Room roomA = new Room();
+            roomA.RoomAmentitiesID = 1;
+            Assert.Equal(1, roomA.RoomAmentitiesID);
+        }
+        [Fact]
+        public void SetRoomAmenitiesID()
+        {
+            Room roomA = new Room();
+            roomA.RoomAmentitiesID = 1;
+            roomA.RoomAmentitiesID = 2;
+            Assert.Equal(2, roomA.RoomAmentitiesID);
+        }
+        [Fact]
+        public async void CanCreateRoom()
+        {
+            DbContextOptions<HotelManagementDbContext> options =
+                new DbContextOptionsBuilder<HotelManagementDbContext>
+                ().UseInMemoryDatabase("RoomAmenity").Options;
+
+            using (HotelManagementDbContext context = new HotelManagementDbContext(options))
+            {
+                // arrange
+                Room roomA = new Room();
+                roomA.ID = 1;
+                roomA.Name = "basic";
+                roomA.RoomAmentitiesID = 1;
+                               
+
+                // Act
+                RoomManagementService Service = new RoomManagementService(context);
+                await Service.CreateRoom(roomA);
+                var result = context.Room.Any(h => h.Name == roomA.Name);
+
+
+                // Assert
+                Assert.True(result);
+            }
+
+        }
+        [Fact]
+        public async void CanUpdateRoom()
+        {
+            DbContextOptions<HotelManagementDbContext> options =
+                new DbContextOptionsBuilder<HotelManagementDbContext>
+                ().UseInMemoryDatabase("RoomAmenity").Options;
+
+            using (HotelManagementDbContext context = new HotelManagementDbContext(options))
+            {
+                // arrange
+                Room roomA = new Room();
+                roomA.ID = 1;
+                roomA.Name = "basic";
+                roomA.RoomAmentitiesID = 1;
+
+
+                // Act
+                RoomManagementService Service = new RoomManagementService(context);
+                await Service.CreateRoom(roomA);
+                roomA.RoomAmentitiesID = 2;
+                Service.UpdateRoom(roomA);
+             
+
+
+                // Assert
+                Assert.Equal(2, roomA.RoomAmentitiesID);
+            }
+
+        }
+        [Fact]
+        public async void CanDeleteRoom()
+        {
+            DbContextOptions<HotelManagementDbContext> options =
+                new DbContextOptionsBuilder<HotelManagementDbContext>
+                ().UseInMemoryDatabase("DeleteRoom").Options;
+
+            using (HotelManagementDbContext context = new HotelManagementDbContext(options))
+            {
+                // arrange
+                Room roomA = new Room();
+                roomA.ID = 1;
+                roomA.Name = "basic";
+                roomA.RoomAmentitiesID = 1;
+
+
+                // Act
+                RoomManagementService Service = new RoomManagementService(context);
+                await Service.CreateRoom(roomA);
+
+                Service.DeleteRoom(roomA);
+                var result = context.Room.FirstOrDefault(m => m.ID == roomA.ID);
+
+
+                // Assert
+                Assert.Null(result);
+            }
+
         }
 
     }
 }
+    
